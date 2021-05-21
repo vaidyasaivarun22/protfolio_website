85080846668cf025113a61e36bb1d7a7bb7e5934
rajasekhar <rajasekhar.b237@gmail.com>
2021-05-21 22:39:37 +0530

    Last commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
